84c1af477468d2fc15bbfd2352bc2dccc5b7ce92
梨花压海棠 <65900880+iam0xcc@users.noreply.github.com>
2020-05-26 10:22:14 +0800

    log

---
+++ windbgPy/Log.py
@@ -0,0 +1,24 @@
+# -*- coding: UTF-8 -*-
+#记录日志文件
+import time
+import pykdPackage
+
+def logBinary(fineName,addrStart,size):
+    '从addrStart拷贝size个字节，以二进制的形式'
+    strtime = time.ctime()
+    strtime = strtime.replace(':', ' ')
+    fileNameTemp = '%s.bin' % strtime
+    fileNameTemp=fineName+fileNameTemp
+    dump = pykdPackage.loadBytes(int(addrStart, 16), int(size, 16))
+    f = open(fileNameTemp, 'wb+')
+    for a in dump:
+        xx = int(a)
+        b = chr(xx)
+        f.write(b)
+    f.close()
+
+def logStr(fileName,content):
+    '把content写入到文件中'
+    f = open(fileName, 'a')
+    f.write(content)
+    f.close()
